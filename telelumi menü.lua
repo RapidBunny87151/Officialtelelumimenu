498edd32e3966b6205bbb54143a90261f8e0e6ae
telelumi <47081687+RapidBunny87151@users.noreply.github.com>
2026-01-18 21:36:11 +0100

    Update FIX

--- telelumi menü.lua
+++ telelumi menü.lua
@@ -49,6 +49,7 @@ if not getgenv().SIRIUS_LOAD_UI_SAFE then
             Instance.new("Frame", ScreenGui).Name = "Playerlist"
             Instance.new("Frame", ScreenGui).Name = "Home"
             Instance.new("Frame", ScreenGui).Name = "Music"
+            Instance.new("Frame", ScreenGui).Name = "LumiiPremiumHub"
             Instance.new("Frame", ScreenGui).Name = "Settings"
             Instance.new("Frame", ScreenGui).Name = "SmartBar"
             Instance.new("ImageButton", ScreenGui).Name = "Toggle"
